256bde6dca1c617047235d3c037f88235cbb4444
R51 <rangers51@gmail.com>
2026-08-10 09:53:39 -0500

    Updating feedback from JSON training

--- src/index.ts
+++ src/index.ts
@@ -951,12 +951,13 @@ client.on('interactionCreate', async (interaction) => {
       await interaction.deferReply();
       const clean = interaction.options.getBoolean('clean') ?? true;
       const trainingJSON = interaction.options.getAttachment('json');
+      const reply = (await interaction.fetchReply()) as Discord.Message; // Must fetch the reply ASAP
 
       if (trainingJSON) {
         const responseMessage = await trainFromAttachmentJson(trainingJSON.url, interaction, clean);
-        await interaction.followUp(responseMessage);
+        // Send a message in reply to the reply to avoid the 15 minute webhook token timeout
+        await reply.reply({ content: responseMessage });
       } else {
-        const reply = (await interaction.fetchReply()) as Discord.Message; // Must fetch the reply ASAP
         const responseMessage = await saveGuildMessageHistory(interaction, clean);
         // Send a message in reply to the reply to avoid the 15 minute webhook token timeout
         await reply.reply({ content: responseMessage });
